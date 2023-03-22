284e004cb5c8ae98fc0dffcfac66b95dc4ff344f
Max Rohivskyi <maxrogivsky@gmail.com>
2023-03-22 17:00:28 +0200

    update screens

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import { useCallback } from "react";
 import { useFonts } from "expo-font";
 
