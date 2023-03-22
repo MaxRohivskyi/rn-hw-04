cd42e7b0f478a87c1ee8136f969ee37d92b67d22
Max Rohivskyi <maxrogivsky@gmail.com>
2023-03-22 17:07:48 +0200

    navigate

--- App.js
+++ App.js
@@ -1,35 +1,24 @@
-import React from "react";
-import { View } from "react-native";
-import { useCallback } from "react";
-import { useFonts } from "expo-font";
+// import { useCallback } from "react";
+// import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+// SplashScreen.preventAutoHideAsync();
 
-import Login from "./screens/LoginScreen";
-import Register from "./screens/RegistrationScreen";
-import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
+import { NavigationContainer } from "@react-navigation/native";
+import { useRoute } from "./router";
 
 const App = () => {
-  const [fontsLoaded] = useFonts({
-    Roboto: require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
-  });
+  // const [fontsLoaded] = useFonts({
+  //   "Roboto": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+  // });
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
 
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      {/* <Login /> */}
-      <Register />
-    </View>
-  );
+  const routing = useRoute(false);
+  return <NavigationContainer>{routing}</NavigationContainer>;
 };
 
 export default App;
